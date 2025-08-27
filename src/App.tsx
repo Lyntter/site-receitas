ca96c834f4af223cf52cae868d3605a40cf2384d
Lyntter <50527406+Lyntter@users.noreply.github.com>
2025-08-27 19:36:28 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 import Header from './components/header'
+import Footer from './components/footer'
+import Grid from './components/grid'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Grid/>
+      <Footer/>  
     </>
   )
 }
